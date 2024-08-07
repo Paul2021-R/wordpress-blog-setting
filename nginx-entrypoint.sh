cae866c00feb91434c18ec59d785a88ea0e31b2e
Hansol Ryu <ryu.axel@gmail.com>
2024-08-07 22:19:50 +0900

    finish : wordpress 설정 완료 - 기본 설정 마무리 - nginx 는 초기에는 80 포트만 열고 열어야 에러 없이 certot이 동작함 - 443 열기도 마무리

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 환경 변수를 사용하여 Nginx 설정 파일 생성
-envsubst '${DOMAIN}' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+envsubst 'archive-paul.serveblog.net' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 
 # Nginx 실행
 nginx -g 'daemon off;'
